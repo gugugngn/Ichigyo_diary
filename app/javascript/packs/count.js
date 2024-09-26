adcc778452f3f4dc0a61c6b3de7489e54169f36d
gugugngn <173164252+gugugngn@users.noreply.github.com>
2024-09-26 01:51:39 +0000

    新規投稿の文字数カウント機能追加

---
+++ app/javascript/packs/count.js
@@ -0,0 +1,26 @@
+// 警告文が気になるためimport〜を追記↓
+import $ from 'jquery';
+
+// 文字数がオーバー(40文字以上)すると表示が赤くなる
+$(function (){
+  var count = $(".js-text").val().length;
+  var now_count = 40 - count;
+
+  if (count > 40) {
+    $(".js-text-count").css("color","red");
+  }
+  
+  $(".js-text-count").text("残り" + now_count + "文字");
+
+  $(".js-text").on("keyup", function() {
+    var count = $(this).val().length;
+    var now_count = 40 - count;
+
+    if (count > 40) {
+      $(".js-text-count").css("color","red");
+    } else {
+      $(".js-text-count").css("color","black");
+    }
+    $(".js-text-count").text("残り" + now_count + "文字");
+  });
+});
