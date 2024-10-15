97904ccdcd5fa6f5e4f04c593d870cac45d93646
gugugngn <173164252+gugugngn@users.noreply.github.com>
2024-10-15 13:08:46 +0000

    検索結果画面にページネーション追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,24 +3,24 @@ class Public::SearchesController < ApplicationController
   before_action :ensure_normal_user
   
   def search
-    @model = params[:model]
-    @content = params[:content]
-    
-    if @model  == "user"
-      if params[:content].present?
-        @users = User.where('name LIKE ?', "%#{@content}%")
-      else
-        @users = User.all
-      end
+  @model = params[:model]
+  @content = params[:content].to_s.strip  # 検索キーワードの前後の空白を削除
+
+  if @model == "user"
+    if @content.blank?
+      redirect_to request.referer, notice: "検索キーワードを入力してください。"
     else
-      if params[:content].present?
-        @posts = Post.where('body LIKE ?', "%#{@content}%").order(created_at: :desc)
-        @grouped_posts = @posts.group_by { |post| post.created_at.to_date }
-      else
-        @posts = Post.all
-      end
+      @users = User.where('name LIKE ?', "%#{@content}%").order(created_at: :desc).page(params[:page])
+    end
+  else
+    if @content.blank?
+      redirect_to request.referer, notice: "検索キーワードを入力してください。"
+    else
+      @posts = Post.where('body LIKE ?', "%#{@content}%").order(created_at: :desc).page(params[:page])
+      @grouped_posts = @posts.group_by { |post| post.created_at.to_date }
     end
   end
+end
   
 
 end
