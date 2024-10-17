33c1eb38f8de9d94366bb1ed03e256ef59453494
gugugngn <173164252+gugugngn@users.noreply.github.com>
2024-10-17 10:12:41 +0000

    管理者自動ログアウト機能追加等

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :logout_user_if_accessing_admin
+  before_action :logout_admin_if_accessing_user
   
   protected
 
@@ -9,10 +10,19 @@ class ApplicationController < ActionController::Base
     end
   end
   
+  # ユーザーでログインしたまま管理者でログインさせないため記述↓
   def logout_user_if_accessing_admin
-    if user_signed_in? && request.path.start_with?(new_admin_session_path)
+    if user_signed_in? && (request.path == new_admin_session_path)
       sign_out(current_user)
       redirect_to new_admin_session_path, alert: "管理者ログインの際は、ユーザーをログアウトする必要があります。ユーザーをログアウトしました。"
+    end
+  end
+  
+  # 管理者でログインしたままユーザーでログインさせないため記述↓
+  def logout_admin_if_accessing_user
+    if admin_signed_in? && (request.path == new_user_session_path || request.path == new_user_registration_path)
+      sign_out(current_admin)
+      redirect_to new_user_session_path, alert: "管理者からログアウトしました。"
     end
   end
   
